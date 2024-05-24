56bea68c024d2107af4c4fb208ac0846a0c27a92
geunhokinn <gghh1220@gmail.com>
2024-05-24 11:39:42 +0900

    feat: 스프링 데이터 Jpa 레파지토리 구성 및 실행 추가

---
+++ src/main/java/hello/itemservicedb/config/SpringDataJpaConfig.java
@@ -0,0 +1,27 @@
+package hello.itemservicedb.config;
+
+import hello.itemservicedb.repository.ItemRepository;
+import hello.itemservicedb.repository.jpa.JpaItemRepositoryV2;
+import hello.itemservicedb.repository.jpa.SpringDataJpaItemRepository;
+import hello.itemservicedb.service.ItemService;
+import hello.itemservicedb.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SpringDataJpaConfig {
+
+    private final SpringDataJpaItemRepository springDataJpaItemRepository;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepositoryV2(springDataJpaItemRepository);
+    }
+}
